15514c107ec3566a08af58f3e4889b16068e6911
Lozii__ <121027576+Loziiif@users.noreply.github.com>
2025-10-07 18:07:40 +0800

    lab3

--- ecommerce/shop_app/views.py
+++ ecommerce/shop_app/views.py
@@ -2,28 +2,40 @@ from dbm import sqlite3
 import os
 from django.conf import settings
 from django.core.paginator import Paginator
+from django.db import connection
 from django.shortcuts import get_object_or_404, render
 from .models import Category, Product  # <- Үүнийг заавал нэмнэ
 import sqlite3 as sql
 
-# def index(request):
-    # categories = Category.objects.all()  
-    # popular_products = Product.objects.all()[:8]  
-    # context = {
-    #     'categories': categories,
-    #     'popular_products': popular_products
-    # }
-    # return render(request, 'index.html', context)
-
 def index(request):
-    return render(request, 'index.html')
+    categories = Category.objects.all()  
+    popular_products = Product.objects.all()[:4]  
+    context = {
+        'categories': categories,
+        'popular_products': popular_products
+    }
+    return render(request, 'index.html', context)
+
+
+
+def store(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT id, category_name FROM category")
+        categories = cursor.fetchall()
+
+        cursor.execute("SELECT id, product_name, price, images FROM product")
+        popular_products = cursor.fetchall()
+
+    context = {
+        'categories': categories,
+        'popular_products': popular_products
+    }
+    return render(request, 'store.html', context)
+
+
 
 def cart_view(request):
     return render(request, 'cart.html')
-def store(request):
-    
-    return render(request, 'store.html')
-
 
 def signin(request):
     return render(request, 'signin.html')
